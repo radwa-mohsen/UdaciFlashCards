c3cc0ed353c469de4973c4c2a0ca34c97a2ecdef
radwa.mohsen <radwa@gfxway.com>
2020-05-03 08:23:16 +0200

    missing redux package and first ui phrase

--- App.tsx
+++ App.tsx
@@ -1,19 +1,40 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, StatusBar } from "react-native";
+import { Provider } from "react-redux";
+import Constants from "expo-constants";
+import { blue } from "./utils/colors";
+import reducer from "./reducers";
+import middleware from "./middleware";
+import { createStore } from "redux";
 
-export default function App() {
+const store = createStore(reducer, middleware);
+
+function FlashCardsStatusBar({ backgroundColor, ...props }) {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View style={{ backgroundColor, height: Constants.statusBarHeight }}>
+      <StatusBar translucent backgroundColor={backgroundColor} {...props} />
     </View>
   );
+}
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <FlashCardsStatusBar backgroundColor={blue} barStyle="light-content" />
+        <View>
+          <Text>Flash Cards App</Text>
+        </View>
+      </Provider>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
