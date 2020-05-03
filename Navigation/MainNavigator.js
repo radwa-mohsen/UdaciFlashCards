7e6619810d930e749de99d64b725692157af8d4b
radwa.mohsen <radwa@gfxway.com>
2020-05-03 09:58:59 +0200

    add icons to tabs

--- Navigation/MainNavigator.js
+++ Navigation/MainNavigator.js
@@ -3,15 +3,36 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Decks from "../components/Decks";
 import AddDeck from "../components/AddDeck";
+import { FontAwesome, MaterialIcons } from "@expo/vector-icons";
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        initialRouteName="Decks"
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ color }) => {
+            if (route.name === "Decks") {
+              return <FontAwesome name="list-alt" size={30} color={color} />;
+            } else if (route.name === "Add Deck") {
+              return (
+                <MaterialIcons name="library-add" size={30} color={color} />
+              );
+            }
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: "tomato",
+          inactiveTintColor: "gray",
+          tabStyle: {
+            marginTop: 8,
+          },
+        }}
+      >
         <Tab.Screen name="Decks" component={Decks} />
-        <Tab.Screen name="AddDeck" component={AddDeck} />
+        <Tab.Screen name="Add Deck" component={AddDeck} />
       </Tab.Navigator>
     </NavigationContainer>
   );
